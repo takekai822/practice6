92bf5c247681209c43f055763e27254687e16ca6
takekai822 <kaikisoccer78@gmail.com>
2022-05-08 05:27:53 +0000

    [Finish]add-home-book

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,13 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+  has_many :books, dependent: :destroy
+
+  has_one_attached :profile_image
+  validates :name, uniqueness: true, length: { minimum: 2, maximum: 20 }
+  validates :introduction, length: { maximum: 50 }
+
+  def get_profile_image
+    (profile_image.attached?) ? profile_image : "no_image.jpg"
+  end
 end
