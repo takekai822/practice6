4d8fac7169203e101fce183a2c4a7b09f84be976
takekai822 <kaikisoccer78@gmail.com>
2022-05-09 08:29:42 +0000

    [Finish]task8a

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'homes#top'
   get 'home/about' => 'homes#about'
+  
   devise_for :users
   resources :books, except: [:new] do
     resource :favorites, only: [:create, :destroy]
@@ -14,5 +15,7 @@ Rails.application.routes.draw do
   end
 
   get 'search' => "searches#search"
+  
+  resources :messages, only: [:create, :show]
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
