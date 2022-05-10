de06de9d570281b0e5ed208c4dae2bd17c334170
takekai822 <kaikisoccer78@gmail.com>
2022-05-10 09:00:33 +0000

    [Finish]add-guestlogin

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,6 @@
 class UsersController < ApplicationController
   before_action :ensure_correct_user, only: [:edit, :update]
+  before_action :ensure_guest_user, only: [:edit, :update]
 
   def index
     @users = User.all
@@ -33,6 +34,13 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
     if @user != current_user
       redirect_to user_path(current_user)
+    end
+  end
+  
+  def ensure_guest_user
+    @user = User.find(params[:id])
+    if @user.name == 'guestuser'
+      redirect_to user_path(current_user), notice: "ゲストユーザーはプロフィール編集画面へ遷移できません。"
     end
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -19,4 +19,7 @@ Rails.application.routes.draw do
   resources :messages, only: [:create, :show]
   get "search_tag" => "books#search_tag"
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  devise_scope :user do
+    post 'users/guest_sign_in', to: 'users/sessions#guest_sign_in'
+  end
 end
